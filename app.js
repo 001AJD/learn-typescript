9f3a80df9c4495df24518898d505229c982eb377
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-11-17 01:54:59 +0530

    learning TypeScript

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 10;
+var num2 = 20;
+console.log(add(10, 20));
+console.log(add(num1, num2));
---
+++ app.ts
@@ -0,0 +1,9 @@
+function add(num1: number, num2: number) {
+	return num1 + num2;
+}
+
+const num1 = 10;
+const num2 = 20;
+
+console.log(add(10, 20));
+console.log(add(num1, num2));
